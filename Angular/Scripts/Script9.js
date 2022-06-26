30366c0e13afab568780d05ea1beab6428276e8c
akiladevi0706 <107547431+akiladevi0706@users.noreply.github.com>
2022-06-26 14:28:55 +0530

    Added the recently worked files

---
+++ Angular/Scripts/Script9.js
@@ -0,0 +1,29 @@
+﻿var app = angular
+            .module("Angular", ["ngRoute"])
+            .config(function ($routeProvider) {
+                $routeProvider
+                    .when("/home", {
+                        templateUrl: "Template/home.html",
+                        controller: "homeController"
+                    })
+                    .when("/courses", {
+                        templateUrl: "Template/courses.html",
+                        controller: "coursesController"
+                    })
+                    .when("/students", {
+                        templateUrl: "Template/students.html",
+                        controller: "studentsController"
+                    })
+            })
+            .controller("homeController", function ($scope) {
+                $scope.message = "Home Page";
+            })
+            .controller("coursesController", function ($scope) {
+                $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server"];
+            })
+             .controller("studentsController", function ($scope, $http) {
+                 $http.get("StudentService.asmx/GetAllStudents")
+                                        .then(function (response) {
+                                            $scope.students = response.data;
+                                        })
+             })
